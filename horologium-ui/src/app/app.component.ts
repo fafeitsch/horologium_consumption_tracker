d73a6ca9f945ff66010ae8f433565f2ecc732bea
Fabian Feitsch <info@fafeitsch.de>
2020-01-05 13:41:36 +0100

    client can fetch data from the graphql server

--- horologium-ui/src/app/app.component.ts
+++ horologium-ui/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import gql from 'graphql-tag';
+import {Apollo} from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'horologium-ui';
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit(): void {
+    this.apollo.query({
+      query: gql`query {
+        allSeries{name}
+      }`
+    }).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+
 }
--- horologium-ui/src/app/graphql.module.ts
+++ horologium-ui/src/app/graphql.module.ts
@@ -2,14 +2,37 @@ import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import {environment} from '../environments/environment';
+import {ApolloLink} from 'apollo-link';
+import {setContext} from 'apollo-link-context';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = environment.graphQlServer; // <-- add the URL of the GraphQL server here
+
 export function createApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8',
+      'Content-Type': 'application/json'
+    }
+  }));
+
+  // Get the authentication token from local storage if it exists
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyTmFtZSI6ImpvaG4ifQ.YAhFVqwtUicCpXp0IUYYx04Lv9Nt9t39UoufA4-Ic_E';
+  const auth = setContext((operation, context) => ({
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  }));
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({uri})]);
+  const cache = new InMemoryCache();
+
   return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
+    link,
+    cache,
   };
 }
+
 
 @NgModule({
   exports: [ApolloModule, HttpLinkModule],
@@ -21,4 +44,5 @@ export function createApollo(httpLink: HttpLink) {
     },
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule {
+}
